f5db64683b58f022722d6ab00caaef1df9c0095d
Phoenix1186 <yolahamisu7@gmail.com>
2025-11-24 17:09:09 +0100

    Update environment variables for channels and groups

--- info.py
+++ info.py
@@ -106,7 +106,7 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 #No_result
 NO_RESULTS_MSG = bool(environ.get('NO_RESULTS_MSG', True))
-NO_RESULTS_CHANNEL = int(environ.get("NO_RESULTS_CHANNEL", "-1001615904511"))
+NO_RESULTS_CHANNEL = int(environ.get("NO_RESULTS_CHANNEL", "-1002450940556"))
 
 # Others
 
@@ -115,13 +115,13 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 PORT = os.environ.get("PORT", "8080")
 MAX_BTN = int(environ.get('MAX_BTN', "10"))
-S_GROUP = environ.get('S_GROUP',"https://t.me/ph0enix_web")
-MAIN_CHANNEL = environ.get('MAIN_CHANNEL',"https://t.me/ph0enix_web")
-FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/ph0enix_web")
+S_GROUP = environ.get('S_GROUP',"https://t.me/mfilerequest")
+MAIN_CHANNEL = environ.get('MAIN_CHANNEL',"https://t.me/Phoenixmoviesfiles")
+FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/Farmersbride")
 MSG_ALRT = environ.get('MSG_ALRT', '𝑪𝑯𝑬𝑪𝑲 & 𝑻𝑹𝒀 𝑨𝑳𝑳 𝑴𝒀 𝑭𝑬𝑨𝑻𝑼𝑹𝑬𝑺...')
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001854325233'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ph0enix_web')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002450940556'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mfilesupportgroup')
 AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
